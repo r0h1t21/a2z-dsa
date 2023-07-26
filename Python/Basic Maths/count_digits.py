4c7e67366dcc6e6ace919f415fe17ac97aa06fe5
Rohit Mallireddy <manideepmallireddy@gmail.com>
2023-07-27 01:58:20 +0530

    Basic Maths

--- Python/Basic Maths/count_digits.py
+++ Python/Basic Maths/count_digits.py
@@ -1,13 +1,12 @@
 import math
 
-'''
-    Extract digits and calculate the count
-    Time Complexity = O(log n)
-    Space Complexity = O(1)
-'''
-
 
 def count_digits_method_1(n):
+    '''
+        Extract digits and calculate the count
+        Time Complexity = O(log n)
+        Space Complexity = O(1)
+    '''
     num = n
     count = 0
     while num > 0:
@@ -19,27 +18,24 @@ def count_digits_method_1(n):
     return count
 
 
-'''
-    Calculate the step (greatest integer function) of log base 10 of the number which would give the
-    number of digits (Since logarithm base 10 of any value is nothing but the exponent to which 10
-    must be raised to yield the given number)
-    Time Complexity = O(1)
-    Space Complexity = O(1)
-'''
-
-
 def count_digits_method_2(n):
+    '''
+        Calculate the step (greatest integer function) of log base 10 of the number which would give the
+        number of digits (Since logarithm base 10 of any value is nothing but the exponent to which 10
+        must be raised to yield the given number)
+        Time Complexity = O(1)
+        Space Complexity = O(1)
+    '''
+
     return math.floor(math.log10(n) + 1)
 
 
-'''
-    Convert the integer to string and calculate length of the string
-    Time Complexity = O(1)
-    Space Complexity = O(1)
-'''
-
-
 def count_digits_method_3(n):
+    '''
+        Convert the integer to string and calculate length of the string
+        Time Complexity = O(1)
+        Space Complexity = O(1)
+    '''
     num = str(n)
 
     return len(num)
---
+++ Python/Basic Maths/reverse_integer.py
@@ -0,0 +1,68 @@
+'''
+    Solution for the problem: https://leetcode.com/problems/reverse-integer/
+'''
+
+
+def reverse_method_1(n):
+    '''
+        Time Complexity = O(log n)
+        Space Complexity = O(1)
+    '''
+    reverse = 0
+    num = n
+    if n < 0:
+        num = 0 - n
+    while num != 0:
+        digit = num % 10
+        '''
+            1) Checking for overflow
+                i) If "reversed" is already "greater than INT32_MAX / 10", multiplying it by 10 and 
+                adding a digit would result in overflow.
+                ii) If "reversed" is "equal to INT32_MAX / 10", the last digit should not exceed 7.
+            2) Here we are not checking for negative numbers because in Python, integer division and modulus for negative
+            numbers behave differently when compared with C/C++/JAVA/JavaScript.
+                i) In Python,
+                    mod = n - math.floor(n/base) * base
+                ii) In C/C++/JAVA/JavaScript,
+                    mod = n - int(n/base) * base
+        '''
+        if reverse > (2**31 - 1) // 10 or (reverse == (2**31 - 1) // 10 and digit > 7):
+            return 0
+        reverse = reverse * 10 + digit
+        num = num // 10
+    if n < 0:
+        reverse = 0 - reverse
+
+    return reverse
+
+
+def reverse_method_2(n):
+    '''
+        Time Complexity = O(log n)
+        Space Complexity = O(log n)
+    '''
+    if n < 0:
+        num = str(0 - n)
+    else:
+        num = str(n)
+    num_list = list(num)
+    for i in range(len(num_list) // 2):
+        num_list[i], num_list[len(
+            num_list) - i - 1] = num_list[len(num_list) - i - 1], num_list[i]
+    num = "".join(num_list)
+    reverse = int(num)
+    if reverse < 2**31 - 1 and reverse > -2**31:
+        if n < 0:
+            return 0 - reverse
+        else:
+            return reverse
+    else:
+        return 0
+
+
+print("Enter the number:")
+n = int(input())
+reverse_num_1 = reverse_method_1(n)
+print("Method 1 Result:", reverse_num_1)
+reverse_num_2 = reverse_method_2(n)
+print("Method 2 Result:", reverse_num_2)
