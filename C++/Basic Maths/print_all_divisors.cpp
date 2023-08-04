63023f3f0eb1f53fcb9d1022a9b1a7d4712adfd8
Rohit Mallireddy <manideepmallireddy@gmail.com>
2023-08-04 11:19:09 +0530

    print_all_divisors updated

--- C++/Basic Maths/print_all_divisors.cpp
+++ C++/Basic Maths/print_all_divisors.cpp
@@ -34,6 +34,41 @@ void method_2(int n)
     }
 }
 
+void method_2_sorted(int n)
+{
+    /*
+        To sort the result we need to use a vector, because we don't know the number of divisors of a number
+        beforehand.
+        The time complexity for each part of the code would be,
+            1) O(√n) for the loop to fetch all the divisors
+            2) O(mlogm) for sorting the vector of divisors, where "m" is the number of divisors
+            3) O(m) for printing all the divisors, where "m" is the number of divisors
+        Since number of divisors (m) is bounded by 2√n, i.e., m <= 2√n (because we iterate until √n and get half of the divisors)
+        So, the total time complexity would be O(√n) + O(mlogm) + O(m),
+            => O(√n) + O(2√nlog(2√n)) + O(2√n)
+            => O(√n + √nlogn), after getting rid of constants
+            => O(√nlogn)
+        The total space complexity would be O(m) for the vector of divisors
+            => O(2√n)
+            => O(√n), after getting rid of constants
+        Time Complexity = O(√nlogn)
+        Space Complexity = O(√n)
+    */
+    vector<int> divisors;
+    for (int i = 1; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            divisors.push_back(i);
+            if (i != n / i)
+                divisors.push_back(n / i);
+        }
+    }
+    sort(divisors.begin(), divisors.end());
+    for (auto i : divisors)
+        cout << i << " ";
+}
+
 int main()
 {
     int n;
@@ -44,6 +79,9 @@ int main()
     cout << endl;
     cout << "Method 2 Result: ";
     method_2(n);
+    cout << endl;
+    cout << "Method 2 Result (Sorted): ";
+    method_2_sorted(n);
 
     return 0;
 }
--- Python/Basic Maths/print_all_divisors.py
+++ Python/Basic Maths/print_all_divisors.py
@@ -25,9 +25,40 @@ def method_2(n):
                 print(n // i, end=" ")
 
 
+def method_2_sorted(n):
+    '''
+        To sort the result we need to use a list, 
+        The time complexity for each part of the code would be,
+            1) O(√n) for the loop to fetch all the divisors
+            2) O(mlogm) for sorting the list of divisors, where "m" is the number of divisors
+            3) O(m) for printing all the divisors, where "m" is the number of divisors
+        Since number of divisors (m) is bounded by 2√n, i.e., m <= 2√n (because we iterate until √n and get half of the divisors)
+        So, the total time complexity would be O(√n) + O(mlogm) + O(m),
+            => O(√n) + O(2√nlog(2√n)) + O(2√n)
+            => O(√n + √nlogn), after getting rid of constants
+            => O(√nlogn)
+        The total space complexity would be O(m) for the list of divisors
+            => O(2√n)
+            => O(√n), after getting rid of constants
+        Time Complexity = O(√nlogn)
+        Space Complexity = O(√n)
+    '''
+    divisors = []
+    for i in range(1, math.floor(math.sqrt(n)) + 1):
+        if n % i == 0:
+            divisors.append(i)
+            if i != n // i:
+                divisors.append(n // i)
+    divisors.sort()
+    for i in divisors:
+        print(i, end=" ")
+
+
 print("Enter the number:")
 n = int(input())
 print("Method 1 Result:", end=" ")
 method_1(n)
 print("\nMethod 2 Result:", end=" ")
 method_2(n)
+print("\nMethod 2 Result (Sorted):", end=" ")
+method_2_sorted(n)
